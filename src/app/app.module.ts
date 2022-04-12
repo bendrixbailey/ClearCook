e6ccb290bfb13de1cd7a3776e91176b1b3310649
bnb4505 <bnb4505@g.rit.edu>
2022-04-11 22:38:01 -0400

    beginning structural setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
