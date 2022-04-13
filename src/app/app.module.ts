10fe45058ae2fbb9ec9421017ada05e26b7ecd51
Bendrix Bailey <bendrix.bailey@gmail.com>
2022-04-13 15:25:14 -0400

    search bar and toolbar complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input'; 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -30,6 +31,7 @@ import { HomepageComponent } from './homepage/homepage.component';
     MatFormFieldModule,
     MatIconModule,
     ReactiveFormsModule,
+    MatInputModule,
     FormsModule,
   ],
   providers: [],
--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ToolbarComponent implements OnInit {
 
   constructor() { }
-  search: String="";
+  search = "";
 
   ngOnInit(): void {
   }
