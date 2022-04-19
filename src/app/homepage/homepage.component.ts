de827461f6ff91f83fd315b710ece67d60f4b3e5
Bendrix Bailey <bendrix.bailey@gmail.com>
2022-04-19 12:30:28 -0400

    added title image and footer, with more text

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -12,6 +12,12 @@ export class HomepageComponent implements OnInit {
     beefStew, chickenRisotto, beefStew, chickenRisotto
   ];
 
+  public randomTitleImages = [
+    "https://embed.widencdn.net/img/cambriausa/xqejgzkmty/1500x1000px/JSD%20Lakeside_02737036dea-1614-4646-b951-00b90f03dcdd.jpeg?keep=c&crop=yes&u=kdpb2n",
+    "https://www.solakitchens.com/wp-content/uploads/2019/08/island-front-landscape-1.jpg",
+    
+  ]
+
   gridColumns = 2;
 
   constructor() { }
@@ -19,8 +25,12 @@ export class HomepageComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  openRandomRecipe(index:number){
+  openRecipe(recipeId:number){
     
+  }
+
+  saveRecipe(recipeId: number){
+
   }
 
 }
